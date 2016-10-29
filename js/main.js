e2ef482acf1a2fde6a0360deda3b5f5b8c151cee
nisargkolhe <nisargkolhe@gmail.com>
2016-10-28 23:03:13 -0400

    fixed index, again

--- js/main.js
+++ js/main.js
@@ -25,7 +25,7 @@
 
             if(io !== undefined) {
                 // Here you create the connection with the server
-                _socket = io.connect("http://localhost:3000/");
+                _socket = io.connect("http://electionvisualizer.herokuapp.com/");
 
                 // This will listen to the "new tweet" signal everytime
                 // there's a new tweet incoming into the stream
