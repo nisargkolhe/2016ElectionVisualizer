1f4f6affd84f64d6a0026f66171705827bd55002
nisargkolhe <nisargkolhe@gmail.com>
2016-11-01 23:53:45 -0400

    Seperated method to get tweet color

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     app = express(),
     server = require("http").createServer(app),
-	fs = require("fs"),
+    fs = require("fs"),
     io = require("socket.io").listen(server, { log: false }),
     theport = process.env.PORT || 3000,
     Twitter = require("twitter"),
@@ -10,19 +10,19 @@ var express = require('express'),
 
 // listens to the port specified
 server.listen(theport);
-console.log ("http server on port: " + theport);
+console.log("http server on port: " + theport);
 
 app.use(express.static(__dirname + '/public'));
-app.use('/js',express.static(__dirname + '/js'));
-app.use('/img',express.static(__dirname + '/img'));
+app.use('/js', express.static(__dirname + '/js'));
+app.use('/img', express.static(__dirname + '/img'));
 
 
 var tw = new Twitter({
-  consumer_key: process.env.TWITTER_CONSUMER_KEY,
-  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
-}),
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    }),
     stream = null,
     track = "clinton,trump",
     users = [];
@@ -31,7 +31,7 @@ var tw = new Twitter({
  * A listener for a client connection
  */
 io.sockets.on("connection", function(socket) {
-    if(users.indexOf(socket.id) === -1) {
+    if (users.indexOf(socket.id) === -1) {
         users.push(socket.id);
     }
 
@@ -41,7 +41,7 @@ io.sockets.on("connection", function(socket) {
     // Listener when a user emits the "start stream" signal
     socket.on("start stream", function() {
         // The stream will be started only when the 1st user arrives
-        if(stream === null) {
+        if (stream === null) {
             tw.stream("statuses/filter", {
                 track: track
             }, function(s) {
@@ -49,85 +49,83 @@ io.sockets.on("connection", function(socket) {
                 var max = 100;
                 stream.on("data", function(data) {
                     // only broadcast when users are online
-                    if(users.length > 0) {
+                    if (users.length > 0) {
 
                         var tweetObj = {
-                        	color: "#FFFFFF",
-                        	lat: "",
-                        	lon: "",
-                        	size: 0,
-                        	text: "",
-                        	user: "",
-                        	data: data,
-                        	error: {}
+                            color: "#FFFFFF",
+                            lat: "",
+                            lon: "",
+                            size: 0,
+                            text: "",
+                            user: "",
+                            data: data,
+                            error: {}
                         };
 
                         var text = "" + data.text;
                         //console.log(data);
                         //Set block size according to user's followers
-                        
+
 
                         //Decide color if the tweet is democratic or republic
                         var jsonString = JSON.stringify(data);
-                        if((jsonString.toLowerCase().indexOf("rump") !== -1 || jsonString.toLowerCase().indexOf("onald") !== -1) && (jsonString.toLowerCase().indexOf("linton") !== -1 || jsonString.toLowerCase().indexOf("illary") !== -1)){
-	                		tweetObj.color = "#FFFFFF";
+                        tweetObj.color = getTweetColor(jsonString.toLowerCase())''
+                        /*if ((jsonString.toLowerCase().indexOf("rump") !== -1 || jsonString.toLowerCase().indexOf("onald") !== -1) && (jsonString.toLowerCase().indexOf("linton") !== -1 || jsonString.toLowerCase().indexOf("illary") !== -1)) {
+                            tweetObj.color = "#FFFFFF";
+                        } else if (jsonString.toLowerCase().indexOf("rump") !== -1 || jsonString.toLowerCase().indexOf("onald") !== -1) {
+                            tweetObj.color = "#E91D0E";
+                        } else if (jsonString.toLowerCase().indexOf("linton") !== -1 || jsonString.toLowerCase().indexOf("illary") !== -1) {
+                            tweetObj.color = "#00a9e0";
+                        }*/
+
+                        if (data.geo) {
+                            tweetObj.lat = data.geo.coordinates[0];
+                            tweetObj.lon = data.geo.coordinates[1];
+                            if (data.user.followers_count > max)
+                                max = data.user.followers_count;
+
+                            tweetObj.size = 10 + 100 * (data.user.followers_count / max);
+                            tweetObj.text = text;
+                            tweetObj.user = data.user.screen_name;
+                            socket.broadcast.emit("new tweet", tweetObj);
+                            socket.emit("new tweet", tweetObj);
+                        } else if (data.place !== undefined && data.place !== null) { //if no coordinate is provided use Google's Geocoding API
+                            if (data.user.followers_count > max)
+                                max = data.user.followers_count;
+
+                            tweetObj.size = 10 + 100 * (data.user.followers_count / max);
+                            tweetObj.text = text;
+                            tweetObj.user = data.user.screen_name;
+
+
+                            var params = {
+                                'key': process.env.GOOGLE_MAPS_GEOCODING_KEY,
+                                'address': data.place.full_name
+                            };
+                            var options = {
+                                url: 'https://maps.googleapis.com/maps/api/geocode/json',
+                                qs: params
+                            };
+                            request.get(options, function(error, response, body) {
+                                if (error)
+                                    console.log(error);
+                                else {
+                                    if (JSON.parse(response.body).status == "OK") {
+                                        tweetObj.lat = JSON.parse(response.body).results[0].geometry.location.lat;
+                                        tweetObj.lon = JSON.parse(response.body).results[0].geometry.location.lng;
+                                        socket.broadcast.emit("new tweet", tweetObj);
+                                        socket.emit("new tweet", tweetObj);
+                                    } else {
+                                        tweetObj.error = JSON.parse(response.body);
+                                        socket.broadcast.emit("new tweet", tweetObj);
+                                        socket.emit("new tweet", tweetObj);
+                                    }
+                                }
+                            });
+
                         }
-	                	else if(jsonString.toLowerCase().indexOf("rump") !== -1 || jsonString.toLowerCase().indexOf("onald") !== -1){
-	                		tweetObj.color = "#E91D0E";
-	                	} else if(jsonString.toLowerCase().indexOf("linton") !== -1 || jsonString.toLowerCase().indexOf("illary") !== -1){
-	                		tweetObj.color = "#00a9e0";
-	                	}
 
-						if(data.geo){
-		                	tweetObj.lat = data.geo.coordinates[0];
-		                	tweetObj.lon = data.geo.coordinates[1];
-		                	if(data.user.followers_count > max)
-	                    		max = data.user.followers_count;
-	                    	
-	                        tweetObj.size = 10 + 100 * (data.user.followers_count/max);
-						    tweetObj.text = text;
-						    tweetObj.user = data.user.screen_name;
-		                	socket.broadcast.emit("new tweet", tweetObj);
-                        	socket.emit("new tweet", tweetObj);
-		                }
-		                else if(data.place !== undefined && data.place !== null){ //if no coordinate is provided use Google's Geocoding API
-	                    	if(data.user.followers_count > max)
-	                    		max = data.user.followers_count;
-	                    	
-	                        tweetObj.size = 10 + 100 * (data.user.followers_count/max);
-						    tweetObj.text = text;
-						    tweetObj.user = data.user.screen_name;
-
-
-		                	var params = {
-								'key': process.env.GOOGLE_MAPS_GEOCODING_KEY,
-								'address': data.place.full_name
-							};
-							var options = {
-								url: 'https://maps.googleapis.com/maps/api/geocode/json',
-								qs: params
-							};
-							request.get(options, function(error, response, body) {
-								if (error) 
-									console.log(error);
-								else {
-									if(JSON.parse(response.body).status=="OK"){
-										tweetObj.lat = JSON.parse(response.body).results[0].geometry.location.lat;
-										tweetObj.lon = JSON.parse(response.body).results[0].geometry.location.lng;
-										socket.broadcast.emit("new tweet", tweetObj);
-	                        			socket.emit("new tweet", tweetObj);
-                        			} else{
-                        				tweetObj.error = JSON.parse(response.body);
-                        				socket.broadcast.emit("new tweet", tweetObj);
-	                        			socket.emit("new tweet", tweetObj);
-                        			}
-								}
-							});
-							
-		                }                        
-		               
-                    }
-                    else {
+                    } else {
                         // If there are no users connected we destroy the stream.
                         console.log("destroying stream");
                         stream.destroy();
@@ -135,23 +133,23 @@ io.sockets.on("connection", function(socket) {
                     }
                 });
                 stream.on('error', function(error) {
-                	console.log("ERROR");
-					console.log(error);
-				});
+                    console.log("ERROR");
+                    console.log(error);
+                });
 
-				stream.on('end', function (response) {
-		            console.log("Twitter stream end");
-				});
+                stream.on('end', function(response) {
+                    console.log("Twitter stream end");
+                });
 
-		        stream.on('destroy', function (response) {
-		            console.log("Twitter stream destroy");
-		        });
-				stream.on('disconnect', function (response) {
-		            console.log("Twitter stream disconnect"+response.disconnect);
-		        });
-            }); 
+                stream.on('destroy', function(response) {
+                    console.log("Twitter stream destroy");
+                });
+                stream.on('disconnect', function(response) {
+                    console.log("Twitter stream disconnect" + response.disconnect);
+                });
+            });
 
-            
+
         }
     });
 
@@ -159,7 +157,7 @@ io.sockets.on("connection", function(socket) {
     socket.on("disconnect", function(o) {
         // find the user in the array
         var index = users.indexOf(socket.id);
-        if(index != -1) {
+        if (index != -1) {
             // Eliminates the user from the array
             users.splice(index, 1);
         }
@@ -172,6 +170,16 @@ io.sockets.on("connection", function(socket) {
         tracking: track
     });
 });
+
+function getTweetColor(String s){
+	if ((s.indexOf("trump") !== -1 || s.indexOf("donald") !== -1) && (s.indexOf("clinton") !== -1 || s.indexOf("hillary") !== -1)) {
+        return = "#FFFFFF";
+    } else if (s.indexOf("trump") !== -1 || s.indexOf("donald") !== -1) {
+        return = "#E91D0E";
+    } else if (s.indexOf("clinton") !== -1 || s.indexOf("hillary") !== -1) {
+        return "#00a9e0";
+    }
+}
 
 function logConnectedUsers() {
     console.log("Users connected: " + users.length);
