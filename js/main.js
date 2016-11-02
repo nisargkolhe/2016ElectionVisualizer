55f12ac218a870a96d4faabd40011c2a0ed3263c
nisargkolhe <nisargkolhe@gmail.com>
2016-11-02 00:00:55 -0400

    fixed stupid Java syntax in JavaScript, dumb.

--- js/main.js
+++ js/main.js
@@ -23,7 +23,7 @@ $(function() {
 
 
     if (io !== undefined) {
-        _socket = io.connect("http://electionvisualizer.herokuapp.com/");
+        _socket = io.connect("http://localhost:3000/");
 
         // This will listen to the "new tweet" signal everytime
         // there's a new tweet incoming into the stream
